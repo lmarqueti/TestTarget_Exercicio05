e05fa70031fdc743f33113570ebeb0c401e6e8e0
lmarqueti <98162109+lmarqueti@users.noreply.github.com>
2023-03-13 19:50:11 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+
+'''
+palavra = "Olá, pessoal!"
+
+palavrainvertida = ""
+
+for i in range(len(palavra)-1, -1, -1):
+    palavrainvertida += palavra[i]
+
+print(palavrainvertida)
+
